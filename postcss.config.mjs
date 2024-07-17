aebb9b6bccaf325e66e1ecdd0b47ca68755b39b6
ju4nqleyton <ju4nqleyton@gmail.com>
2024-07-16 21:07:32 -0500

    repository initialized with vite, ts, tailwind, prettier config done

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
